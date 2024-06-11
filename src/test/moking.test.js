b6515520e8c3861b814938bf65afa977441346b8
SerafimGalinschii <serafim.galinschii@gmail.com>
2024-06-11 14:23:12 +0300

    unit test mock for getShippingInfo function

--- src/test/moking.test.js
+++ src/test/moking.test.js
@@ -1,8 +1,10 @@
 import { vi, it, expect, describe } from "vitest";
-import { getPriceInCurrency } from "../mocking";
+import { getPriceInCurrency, getShippingInfo } from "../mocking";
 import { getExchangeRate } from "../libs/currency";
+import { getShippingQuote } from "../libs/shipping";
 
 vi.mock("../libs/currency.js");
+vi.mock("../libs/shipping.js");
 
 describe("test suite", () => {
   it("tste case 1", () => {
@@ -23,5 +25,19 @@ describe("getPriceInCurrency", () => {
     const price = getPriceInCurrency(10, "USD");
 
     expect(price).toBe(15);
+  });
+});
+
+describe("getShippingInfo", () => {
+  it("should return shipping unavailable", () => {
+    vi.mocked(getShippingQuote).mockReturnValue(null);
+
+    expect(getShippingInfo("USA")).toMatch(/Unavailable/i);
+  });
+  it("should return shipping cost", () => {
+    vi.mocked(getShippingQuote).mockReturnValue({ cost: 10, estimatedDays: 5 });
+
+    expect(getShippingInfo("USA")).toMatch(/5 days/i);
+    expect(getShippingInfo("USA")).toMatch("10");
   });
 });
